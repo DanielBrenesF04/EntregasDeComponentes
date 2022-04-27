2eaed7b9d0091472a5bf01dc978d6afa56afb78e
DanielBrenesF04 <82352147+DanielBrenesF04@users.noreply.github.com>
2022-04-26 23:05:58 -0600

    Lan graphQL

---
+++ EntregasDeComponentes/LabGraph/graphql/src/main/java/com/cenfotec/graphql/graphql/mutation/VehicleMutation.java
@@ -0,0 +1,18 @@
+package com.cenfotec.graphql.graphql.mutation;
+
+import com.cenfotec.graphql.graphql.entities.Vehicle;
+import com.cenfotec.graphql.graphql.services.VehicleService;
+import com.coxautodev.graphql.tools.GraphQLMutationResolver;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import java.time.LocalDate;
+
+@Component
+public class VehicleMutation implements GraphQLMutationResolver {
+@Autowired
+    private VehicleService vehicleService;
+    public Vehicle createVehicle(String type, String modelCode, String brandName,
+                                 String launchDate) {
+        return this.vehicleService.createVehicle(type, modelCode, brandName, launchDate);
+    }
+}
